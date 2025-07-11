577408d2b05bf3c1f3bd658102f7dd84ee15db5f
manel1954 <55663831+manel1954@users.noreply.github.com>
2025-07-11 12:35:41 +0200

    Update actualizar.sh

--- actualizar.sh
+++ actualizar.sh
@@ -6,10 +6,11 @@ if [ "$hotspot_superior" = 'HOTSPOT' ];then
 else
 /home/pi/A108/qt/./qt_menu_superior 
 fi
-
+sleep 3
 /home/pi/A108/qt/./qt_menu_inferior
+sleep 3
 /home/pi/A108/qt/./qt_boton_hotspot_unico
-
+slepp 3
 sudo systemctl disable ysfgateway.service
 sudo systemctl disable dmr2ysf.service
 sudo systemctl disable analog_bridge.service
