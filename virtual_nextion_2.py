e7d0eecd673dab3b0d6554a6a7b5bef0f69b1703
manel1954 <55663831+manel1954@users.noreply.github.com>
2025-07-14 17:26:28 +0200

    Update virtual_nextion_2.py

--- virtual_nextion_2.py
+++ virtual_nextion_2.py
@@ -13,7 +13,7 @@ BAUD_RATE = 9600
 
 # Configuración de la ventana de Tkinter
 WINDOW_TITLE = "MMDVMHost Virtual Nextion"
-WINDOW_SIZE = "614x268+640+590"  # Dimensiones fijas
+WINDOW_SIZE = "614x268+643+590"  # Dimensiones fijas
 WINDOW_BG_COLOR = "#152637"
 
 # Crear ventana principal
